e084ed16db8af41f670271ae0e52a76ff200cfaa
jirikostiha <ijkdata@gmail.com>
2023-01-17 21:31:48 +0100

    feat: serializer2

---
+++ src/code/CellularAutomaton/BoolArray2DSerializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace CellularAutomaton
+{
+    public class BoolArray2DSerializer
+    {
+        public string Separator { get; set; } = Environment.NewLine;
+
+        public string Serialize(IArray2D<bool> array2D)
+        {
+            var builder = new StringBuilder();
+            for (int x = 0; x < array2D.XCount; x++)
+            {
+                for (int y = 0; y < array2D.YCount; y++)
+                {
+                    if (array2D.GetAt(x,y))
+                        builder.Append($"{x},{y}").Append(Separator);
+                }
+            }
+            builder.Remove(builder.Length - Separator.Length, Separator.Length);
+
+            return builder.ToString();
+        }
+
+        public void Deserialize(string content, IArray2D<bool> array2D)
+        {
+            var items = content.Split(Separator);
+            var xmax = array2D.XCount - 1;
+            var ymax = array2D.YCount - 1;
+            foreach (var item in items)
+            {
+                var delimiterIndex = item.IndexOf(',');
+                var x = int.Parse(item.Substring(0, delimiterIndex));
+                var y = int.Parse(item.Substring(delimiterIndex + 1));
+
+                if (x <= xmax && y <= ymax)
+                    array2D.SetAt(x, y, true);
+            }
+        }
+    }
+}
---
+++ src/code/CellularAutomaton/BoolArray2DSerializer2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CellularAutomaton
+{
+    public class BoolArray2DSerializer2
+    {
+        private readonly BoolArray2DSerializer _arraySerializer;
+        private Func<int, int, IArray2D<bool>> _arrayCreator;
+
+        public BoolArray2DSerializer2(Func<int, int, IArray2D<bool>> arrayCreator, BoolArray2DSerializer? underlyinSerializer = null)
+        {
+            _arraySerializer = underlyinSerializer ?? new BoolArray2DSerializer();
+            _arrayCreator = arrayCreator;
+        }
+
+        public string Serialize(IArray2D<bool> array2D)
+            => $"size:{array2D.XCount}x{array2D.YCount}"
+                + Environment.NewLine 
+                + _arraySerializer.Serialize(array2D);
+
+        public IArray2D<bool> Deserialize(string contentWithHeader)
+        {
+            var header = contentWithHeader.Substring(0, contentWithHeader.IndexOf(Environment.NewLine));
+            var size = Regex.Match(header, "size:([\\d]*)x([\\d]*)");
+            var xmax = int.Parse(size.Groups[1].Value);
+            var ymax = int.Parse(size.Groups[2].Value);
+            IArray2D<bool> array2D = _arrayCreator(xmax, ymax);
+            array2D.SetRegion(0,0, xmax-1, ymax-1, false);
+            string body = contentWithHeader.Substring(contentWithHeader.IndexOf(Environment.NewLine) + 1);
+            _arraySerializer.Deserialize(body, array2D);
+
+            return array2D;
+        }
+    }
+}
