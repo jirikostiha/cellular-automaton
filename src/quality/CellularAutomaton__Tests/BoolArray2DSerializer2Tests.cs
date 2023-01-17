3e259498d28581fcc0c01e94c6149aa7d2581684
jirikostiha <ijkdata@gmail.com>
2023-01-17 21:33:06 +0100

    tests: serializer2

---
+++ src/quality/CellularAutomaton__Tests/BoolArray2DSerializer2Tests.cs
@@ -0,0 +1,38 @@
+﻿namespace CellularAutomaton
+{
+    using System;
+    using System.Collections;
+    using System.Linq;
+    using Xunit;
+
+    public class BoolArray2DSerializer2Tests
+    {
+        [Fact]
+        public void Serialize()
+        {
+            var array2D = BitArray2D.Create(4, 11, true, false);
+            var serializer = new BoolArray2DSerializer2((xcount, ycount) =>
+                BitArray2D.Create(xcount, ycount, true));
+
+            var content = serializer.Serialize(array2D);
+            
+            Assert.Equal("size:4x11", content.Split(Environment.NewLine).First());
+        }
+
+        [Fact]
+        public void Deserialize()
+        {
+            var array2D = BitArray2D.Create(4, 11, true, false);
+            var serializer = new BoolArray2DSerializer2((xcount, ycount) =>
+                BitArray2D.Create(xcount, ycount, true));
+            var content = serializer.Serialize(array2D);
+
+            var newArray2D = serializer.Deserialize(content);
+
+            Assert.Equal(4, newArray2D.XCount);
+            Assert.Equal(11, newArray2D.YCount);
+            Assert.False(newArray2D.GetAt(0, 0));
+            Assert.True(newArray2D.GetAt(1, 1));
+        }
+    }
+}
---
+++ src/quality/CellularAutomaton__Tests/BoolArray2DSerializerTests.cs
@@ -0,0 +1,38 @@
+﻿namespace CellularAutomaton
+{
+    using System.Collections;
+    using System.Linq;
+    using Xunit;
+
+    public class BoolArray2DSerializerTests
+    {
+        [Fact]
+        public void Serialize()
+        {
+            var array2D = BitArray2D.Create(4, 4, true, false);
+            var serializer = new BoolArray2DSerializer();
+
+            var content = serializer.Serialize(array2D);
+            
+            Assert.Equal(4, content.Split(serializer.Separator).Length);
+            Assert.Equal("1,1", content.Split(serializer.Separator).First());
+        }
+
+        [Fact]
+        public void Deserialize()
+        {
+            var array2D = BitArray2D.Create(4, 4, true, false);
+            var serializer = new BoolArray2DSerializer();
+            var content = serializer.Serialize(array2D);
+
+            var newArray2D = BitArray2D.Create(4, 4, false);
+            serializer.Deserialize(content, newArray2D);
+
+            Assert.False(newArray2D.GetAt(0, 0));
+            Assert.False(newArray2D.GetAt(3, 3));
+            Assert.True(newArray2D.GetAt(1, 1));
+            Assert.True(newArray2D.GetAt(1, 2));
+            Assert.True(newArray2D.GetAt(2, 2));
+        }
+    }
+}
