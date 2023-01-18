7dea9508eae4fdc91782ef5d2e377bac0f927516
jirikostiha <ijkdata@gmail.com>
2023-01-18 23:11:03 +0100

    test(bench): processor benchmarks

---
+++ src/quality/CellularAutomaton__Benchmarks/ProcessorNextGeneration.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CellularAutomaton
+{
+    [MemoryDiagnoser]
+    public class ProcessorNextGeneration
+    {
+        [Params(1, 10, 100)]
+        public int N;
+
+        [Params(100, 1_000, 10_000)]
+        public int Size;
+
+        private BitArray2D _bitArray2D;
+        private BoolArray2D _boolArray2D;
+
+        private GenerationProcessorOptions _options = new() { CleanBorders = true };
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            _bitArray2D = BitArray2D.Create(Size, Size);
+            _boolArray2D = BoolArray2D.Create(Size, Size);
+        }
+
+        [Benchmark]
+        public void Next_BitArray2D()
+        {
+            var processor = new GenerationProcessor(_bitArray2D, _options);
+            for (int i = 0; i < N; i++)
+            {
+                processor.Next();
+            }
+        }
+
+        [Benchmark]
+        public void Next_BoolArray2D()
+        {
+            var processor = new GenerationProcessor(_boolArray2D, _options);
+            for (int i = 0; i < N; i++)
+            {
+                processor.Next();
+            }
+        }
+    }
+}
---
+++ src/quality/CellularAutomaton__Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace CellularAutomaton
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = BenchmarkRunner.Run<ProcessorNextGeneration>();
+
+            Console.ReadKey();
+        }
+    }
+}
